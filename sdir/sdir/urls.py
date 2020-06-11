6505f09c27a6ca43780fef1e944533e5d09e628a
eturin <eturine@uni.pe>
2020-06-11 00:43:47 -0500

    agregando vista hola mundo!

--- sdir/sdir/urls.py
+++ sdir/sdir/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
+from indicadores.views import demo_vista_basica
 
 urlpatterns = [
+    url(r'', demo_vista_basica),
     path('admin/', admin.site.urls),
+
 ]
