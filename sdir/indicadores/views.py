7e6b5c824e7201aab88e7fb402e3848e1e4a38b8
eturin <eturine@uni.pe>
2020-06-11 21:45:28 -0500

    hola mundo4

--- sdir/indicadores/views.py
+++ sdir/indicadores/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def demo_vista_basica(request):
-    return HttpResponse('Hola mundo3!!')
+    return HttpResponse('Hola mundo4!!')
